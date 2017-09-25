09f5152b7263b72bfecca74ae31ddf14619bd62c
Unknown <rschumaker40@gmail.com>
2017-09-24 22:51:06 -0400

    Restructuring assets tree into modules and adding namespaces to scripts

---
+++ Assets/_Abilities/Fireball/Fireball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+
+namespace RPG.Abilities
+{
+    public class Fireball : MonoBehaviour
+    {
+
+        public Rigidbody rgbd;
+        public GameObject fieryParticle;
+        public GameObject smokeParticle;
+        public GameObject explosionParticle;
+        public float projectileSpeed;
+
+        float damageCaused;
+
+        public void SetDamage(float damage)
+        {
+            damageCaused = damage;
+        }
+
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter(Collider collider)
+        {
+            Component damageableComponent = collider.gameObject.GetComponent(typeof(IDamageable));
+            if (damageableComponent)
+            {
+                (damageableComponent as IDamageable).TakeDamage(damageCaused);
+            }
+
+            fieryParticle.SetActive(false);
+            smokeParticle.SetActive(false);
+            explosionParticle.SetActive(true);
+            rgbd.constraints = RigidbodyConstraints.FreezeAll;
+            Destroy(gameObject.GetComponent(typeof(Collider)));
+            Destroy(gameObject, 2);
+        }
+    }
+}
+
--- Assets/_Core/SpinMe.cs
+++ Assets/_Core/SpinMe.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpinMe : MonoBehaviour {
+namespace RPG.Core
+{
+    public class SpinMe : MonoBehaviour
+    {
 
-	[SerializeField] float xRotationsPerMinute = 1f;
-	[SerializeField] float yRotationsPerMinute = 1f;
-	[SerializeField] float zRotationsPerMinute = 1f;
-	
-	void Update () {
+        [SerializeField] float xRotationsPerMinute = 1f;
+        [SerializeField] float yRotationsPerMinute = 1f;
+        [SerializeField] float zRotationsPerMinute = 1f;
 
-        // xDegreesPerFrame = Time.DeltaTime, 60, 360, xRotationsPerMinute
-        // degrees frame^-1 = seconds frame^-1 / seconds minute^-1 * degrees rotation^-1 * rotation minute^-1
-        // degrees frame^-1 = frame^-1 minute * degrees rotation^-1 * rotation minute^-1
-        // degrees frame^-1 = frame^1- * degrees
+        void Update()
+        {
 
-        float xDegreesPerFrame = Time.deltaTime / 60 * 360 * xRotationsPerMinute;
-        transform.RotateAround (transform.position, transform.right, xDegreesPerFrame);
+            // xDegreesPerFrame = Time.DeltaTime, 60, 360, xRotationsPerMinute
+            // degrees frame^-1 = seconds frame^-1 / seconds minute^-1 * degrees rotation^-1 * rotation minute^-1
+            // degrees frame^-1 = frame^-1 minute * degrees rotation^-1 * rotation minute^-1
+            // degrees frame^-1 = frame^1- * degrees
 
-		float yDegreesPerFrame = Time.deltaTime / 60 * 360 * yRotationsPerMinute; ; // TODO COMPLETE ME
-        transform.RotateAround (transform.position, transform.up, yDegreesPerFrame);
+            float xDegreesPerFrame = Time.deltaTime / 60 * 360 * xRotationsPerMinute;
+            transform.RotateAround(transform.position, transform.right, xDegreesPerFrame);
 
-        float zDegreesPerFrame = Time.deltaTime / 60 * 360 * zRotationsPerMinute; ; // TODO COMPLETE ME
-        transform.RotateAround (transform.position, transform.forward, zDegreesPerFrame);
-	}
+            float yDegreesPerFrame = Time.deltaTime / 60 * 360 * yRotationsPerMinute; ; // TODO COMPLETE ME
+            transform.RotateAround(transform.position, transform.up, yDegreesPerFrame);
+
+            float zDegreesPerFrame = Time.deltaTime / 60 * 360 * zRotationsPerMinute; ; // TODO COMPLETE ME
+            transform.RotateAround(transform.position, transform.forward, zDegreesPerFrame);
+        }
+    }
 }
+
