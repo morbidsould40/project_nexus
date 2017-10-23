6f90e37413debe0914c830d48e715b5546215433
Unknown <rschumaker40@gmail.com>
2017-10-22 22:32:11 -0400

    Set up health and energy fliiable orbs

--- Assets/_Characters/Energy.cs
+++ Assets/_Characters/Energy.cs
@@ -6,7 +6,7 @@ namespace RPG.Characters
 {
     public class Energy : MonoBehaviour
     {
-        [SerializeField] RawImage energyBar = null;
+        [SerializeField] Image energyOrb = null;
         [SerializeField] float maxEnergyPoints = 100f;
         [SerializeField] float regenPointsPerSecond = 10f;
 
@@ -17,7 +17,8 @@ namespace RPG.Characters
         // Use this for initialization
         void Start()
         {
-            currentEnergyPoints = maxEnergyPoints;            
+            currentEnergyPoints = maxEnergyPoints;
+            UpdateEnergyOrb();
         }
 
         void Update()
@@ -32,7 +33,7 @@ namespace RPG.Characters
             {
                 var pointsToAdd = regenPointsPerSecond * Time.deltaTime;
                 currentEnergyPoints = Mathf.Clamp(currentEnergyPoints + pointsToAdd, 0, maxEnergyPoints);
-                UpdateEnergyBar();
+                UpdateEnergyOrb();
             }
         }
 
@@ -45,13 +46,12 @@ namespace RPG.Characters
         {
             float newEnergyPoints = currentEnergyPoints - amount;
             currentEnergyPoints = Mathf.Clamp(newEnergyPoints, 0, maxEnergyPoints);
-            UpdateEnergyBar();
+            UpdateEnergyOrb();
         }
 
-        private void UpdateEnergyBar()
+        private void UpdateEnergyOrb()
         {
-            float xValue = -(EnergyAsPercent() / 2f) - 0.5f;
-            energyBar.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+            energyOrb.fillAmount = EnergyAsPercent();
         }
 
         float EnergyAsPercent()
